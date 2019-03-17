918aafefa72b69fc6fad9c842421892fd50980df
Matthew DuBois <mattrdubois@gmail.com>
2019-03-17 14:44:22 -0500

    added router link to habit edit and fixed habit index bug

--- src/views/Habits.vue
+++ src/views/Habits.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="habits">
+
     <h2>Here are your habits...</h2>
     <br>
     <div v-for="habit in habits">
       <div class="container">
-        <h1>{{ habit.name }}</h1>
+        <router-link v-bind:to="'/habits/' + habit.id + '/edit'"><h1>{{ habit.name }}</h1></router-link>
         <h4> {{ habit.description }}</h4>
         <div v-for="habit_completed in habit.habit_completeds">
           <h5>{{ habit_completed.created_at }}</h5>
@@ -21,31 +22,9 @@
     </div>
   </div>
 
-       <!--  <h2>Temptations</h2>
-      <div class="card-deck">
-        <div class="col-md-4" v-for="temptation in temptations">
-          <router-link v-bind:to="'/temptations/' + temptation.id + '/edit'">
-            <div class="card mt-3 bg-light"><span class="border border-default"></span>
-              <div>
-                <div class="brand-card-header">
-                  <h2 class="card-title mt-3 text-center text-dark" > {{ temptation.name }}</h2>
-                </div>
-                <div class="brand-card">
-                  <div class="brand-card-body">
-                    <div>
-                      <p class="card-text text-center"><small class="text-dark"> ${{ temptation.cost }} </small></p>
-                    </div>
-                    <p></p>                    
-                    <div> 
-                      <p class="card-text text-center"><small class="text-dark"> {{ temptation.time }} </small></p>
-                    </div>  
-                  </div>
-                </div>
-              </div>
-            </div>
-          </router-link>
-        </div>
-      </div> -->
+
+
+   
 </template>
 
 
@@ -66,7 +45,7 @@ export default {
                   }]
 
                 }],
- 
+                
                 
       errors: []
     };
