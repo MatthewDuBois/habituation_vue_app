ab09239b78d2dc2611c5f7667ba4762d349c8acb
Matthew DuBois <mattrdubois@gmail.com>
2019-03-20 12:02:48 -0500

    mvp is done

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -57,6 +57,8 @@ export default {
           axios.defaults.headers.common["Authorization"] =
             "Bearer " + response.data.jwt;
           localStorage.setItem("jwt", response.data.jwt);
+          localStorage.setItem("avatar_id", response.data.avatar_id);
+
           this.$router.push("/avatarhome");
         })
         .catch(error => {
